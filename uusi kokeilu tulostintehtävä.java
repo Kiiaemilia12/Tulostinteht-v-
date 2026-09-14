88d657b64a787bd3f57ca8e97ba0d188bab2fd1a
Kiiaemilia12 <kiiavuori@gmail.com>
2026-09-14 13:50:38 +0300

    Add files via upload

---
+++ uusi kokeilu tulostintehtävä.java
@@ -0,0 +1,30 @@
+public class uusi {
+    public static void main(String[] args) throws Exception {
+        // Code is written here
+        String name = "Kiia";
+        double luku1 = 5;
+        double luku2 = 2;
+
+        System.out.println("Hei olen tulostinohjelma") ;
+        System.out.println("My name is " + name + " Hei olen tulostinohjelma") ;
+
+        System.out.println("luku1") ;
+        System.out.println("luku1 muuttujan arvo on " + luku1) ;
+        System.out.println("luku2 muuttujan arvo on " + luku2) ;
+        System.out.println("luku1 * luku2 = " + (luku1 * luku2)) ;
+        System.out.println("luku1 + luku2 = " + (luku1 + luku2)) ;
+        System.out.println("luku1 - luku2 = " + (luku1 - luku2)) ;
+        System.out.println("luku1 / luku2 = " + (luku1 / luku2)) ;
+
+
+    
+    
+
+        
+    
+
+
+
+
+    }
+}
